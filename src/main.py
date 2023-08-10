8d328882dcb5dbf6c830dc32feafc5c61f0f16f8
Kreker <angelikatum14@gmail.com>
2023-08-10 17:33:50 +0300

    Create factorial.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import modules.perimetr as perimeter
+import modules.factorial as factorial
+
+f1 = factorial.factorial_norm(5)
+print(f1)
+
+f2 = factorial.factorial_norm(12)
+print(f2)
 
 p1 = perimeter.perimetr_func([1, 3, 4])
 print(p1)
