3ccefaaace286192d884c13545f3433c25ce0023
Kreker <angelikatum14@gmail.com>
2023-08-10 16:18:16 +0300

    Bug fix

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import modules.perimetr as perimeter
 
-p1 = perimeter([1, 3, 4])
+p1 = perimeter.perimetr_func([1, 3, 4])
 print(p1)
 
-p2 = perimeter([4, 2, 6, 3, 1])
+p2 = perimeter.perimetr_func([4, 2, 6, 3, 1])
 print(p2)
 
 print('Hello')
