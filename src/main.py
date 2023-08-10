789274b290f4cd7f446184e8ebe4cc069c6a42eb
Kreker <angelikatum14@gmail.com>
2023-08-10 17:47:54 +0300

    Bug fix2

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 import modules.perimetr as perimeter
 import modules.factorial as factorial
 
-f1 = factorial.factorial_rec(5)
-print(f1)
-
-f2 = factorial.factorial_rec(9)
-print(f2)
-
 f1 = factorial.factorial_norm(5)
 print(f1)
 
 f2 = factorial.factorial_norm(12)
 print(f2)
+
+f3 = factorial.factorial_rec(5)
+print(f3)
+
+f4 = factorial.factorial_rec(9)
+print(f4)
 
 p1 = perimeter.perimetr_func([1, 3, 4])
 print(p1)
--- src/modules/factorial.py
+++ src/modules/factorial.py
@@ -2,6 +2,8 @@ def factorial_rec(n):
     if n <= 1:
         return 1
     return n * factorial_rec(n - 1)
+
+
 def factorial_norm(n):
     sum = 1
     for i in range(1, n + 1):
